15f8b63e75db3510e9acdc5ea20d5553a711959b
Jacob Miller <Jamiller219@gmail.com>
2025-09-17 20:09:48 -0500

    init Marp

---
+++ marp_presentations/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A flake for Marp presentations.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.$(system);
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              marp-cli
+              nodejs
+              chromium
+            ];
+
+            shellHook = ''
+              echo "Marp dev environment loaded..."
+            '';
+          };
+      });
+}
